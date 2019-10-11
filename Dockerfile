84df6a5f2aef081d7e948a9bef09d974bdd2093d
Nazmi Altun <nazmialtun@windowslive.com>
2019-10-11 20:17:31 +0800

    issues fixed

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
-FROM node:current-alpine
+FROM hahoo/node-gm:latest
+RUN apt-get update \
+  && apt-get install -y --no-install-recommends graphicsmagick \
+  && apt-get autoremove -y \
+  && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm install
@@ -6,4 +10,5 @@ RUN npm ci --only=production
 COPY . .
 
 EXPOSE 8080
+RUN mkdir Images
 CMD [ "node", "server.js" ]
--- server.js
+++ server.js
@@ -2,14 +2,28 @@
 
 const express = require('express');
 const svgCaptcha = require('svg-captcha');
+const fs = require('fs');
+const gm = require('gm');
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 const app = express();
-app.get('/', (req, res) => {
-    var captcha = svgCaptcha.create();
-    res.status(500).json(captcha);
+app.get('/captcha', (req, res) => {
+    var captcha = svgCaptcha.create({
+        width:100,
+        height:50,
+        noise:0
+    });
+    fs.writeFileSync('./Images/'+ captcha.text + '.svg',captcha.data);
+    gm('./Images/'+ captcha.text + '.svg')
+      .out('-background', 'none')
+      .toBuffer('png',function (err, buffer) {
+        if (err){
+          console.log(err);
+        }
+        res.header('Text', captcha.text).type('png').end(buffer);
+      })
 });
 
 app.listen(PORT, HOST);
